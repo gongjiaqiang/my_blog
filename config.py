e6b6ffe1ac722e817794820a40a5e29e615f5284
huitailang <huitailang@qq.com>
2018-11-26 23:08:27 +0800

    flask搭建简单个人博客未完成版

---
+++ config.py
@@ -0,0 +1,63 @@
+# coding=utf8
+
+import os
+import redis
+from logging.config import dictConfig
+
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+
+class Config(object):
+    """基本配置参数"""
+    SECRET_KEY = "TQ6uZxn+SLqiLgVimX838/VplIsLbEP5jV7vvZ+Ohqw="
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret string'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # 创建redis实例用到的参数
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+
+    # flask-session使用的参数
+    SESSION_TYPE = "redis"  # 保存session数据的地方
+    SESSION_USE_SIGNER = True  # 为session id进行签名
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 保存session数据的redis配置
+    PERMANENT_SESSION_LIFETIME = 86400  # session数据的有效期秒
+
+    @staticmethod
+    def init_app(self):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql://root:mysql@localhost/mysite'
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql://root:mysql@localhost/mysite'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'mysql://root:mysql@localhost/mysite'
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
+
+# dictConfig({
+#     'version': 1,
+#     'formatters': {'default': {
+#         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+#     }},
+#     'handlers': {'wsgi': {
+#         'class': 'logging.StreamHandler',
+#         'stream': 'ext://flask.logging.wsgi_errors_stream',
+#         'formatter': 'default'
+#     }},
+#     'root': {
+#         'level': 'INFO',
+#         'handlers': ['wsgi']
+#     }
+# })
---
+++ my_blog/__init__.py
@@ -0,0 +1,63 @@
+# coding=utf8
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from my_blog.utils.commons import RegexConverter
+from config import config, Config
+from flask_wtf import CSRFProtect
+from flask_session import Session
+import redis
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+# 设置日志的记录等级
+logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+file_log_handler = RotatingFileHandler("logs/all.log", maxBytes=1024*1024*100, backupCount=10)
+# 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象（应用程序实例app使用的）添加日后记录器
+logging.getLogger().addHandler(file_log_handler)
+
+# 创建数据库对象
+db = SQLAlchemy()
+# 创建redis对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+# 使用wtf提供的csrf保护机制
+csrf = CSRFProtect()
+
+
+def create_app(config_name):
+    # 创建app实例的工厂方法
+    app = Flask(__name__)
+
+    # 从配置对象中为app设置配置信息
+    app.config.from_object(config[config_name])
+
+    # 为app中的url路由添加正则表达式匹配
+    app.url_map.converters["regex"] = RegexConverter
+
+    # 初始化配置文件
+    config[config_name].init_app(app)
+
+    # 初始化数据库
+    db.init_app(app)
+
+    # 为app添加CSRF保护
+    csrf.init_app(app)
+
+    # 使用flask-session扩展，用redis保存app的session数据
+    Session(app)
+
+    # 注册蓝图
+    from .blog_1_0 import main as main_print
+    app.register_blueprint(main_print, url_prefix='/api/v1.0')
+
+    # 为app添加返回静态html的蓝图应用
+    from .web_page import html as html_print
+    app.register_blueprint(html_print)
+
+    return app
