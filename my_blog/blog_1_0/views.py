99fddfaaa724af806a3234484d3754480f3e1d7d
Leslie <gongjq@akulaku.com>
2018-11-28 16:40:02 +0800

    正在完成中...

--- my_blog/blog_1_0/views.py
+++ my_blog/blog_1_0/views.py
@@ -6,3 +6,12 @@ from . import main
 @main.route("/")
 def index():
     return render_template('index.html')
+
+
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
